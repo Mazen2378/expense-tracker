b15a038f807a34dee2ced1ac32b7d0de1d6d369c
mazen <mazen.jedidi.23@gmail.com>
2022-10-02 23:26:42 +0100

    improving the calculator

--- src/components/Calcul.tsx
+++ src/components/Calcul.tsx
@@ -1,23 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+const parStack:string[] = []
 const Calcul: React.FC = () => {
-  const [expression, setExpression] = useState<string>('')
-    const [ev, setEv] = useState<string>('0')
+    const [expression, setExpression] = useState<string>('')
+    const [ev, setEv] = useState<number>(0)
     const [lastNum, setLastNum] = useState<string>('')
-  const handleClick = (val:string) => {
-    if (val === '.') {
-        if (lastNum.includes('.')) {
-          return
+    useEffect(() => {
+        /* if (["+", "-", "*", "/", "%", "(",")","."].includes(expression[expression.length - 1]) || expression.length < 1) {
+  *     return
+  * } */
+        try {
+            let res = eval(expression)
+            setEv(res)
+        } catch (err) {
+            console.log(err)
         }
-      setLastNum(lastNum + '.')
-    } else {
-      setLastNum('')
-    }
+        if (expression === '') {
+            setEv(0)
+        }
+    }, [expression])
 
-      if (["+", "-", "*", "/","."].includes(expression[expression.length - 1])) {
-        return
+    const handleClick = (val: string) => {
+      if (val === ')') {
+        if (expression[expression.length -1] === '(') return; 
+        if (parStack.length < 1) {
+          return
+        } else {
+            parStack.pop()
+        }
+      }
+        if (val === '.') {
+            if (lastNum.includes('.')) {
+                return
+            }
+            setLastNum(lastNum + '.')
+        } else {
+            setLastNum('')
+        }
+        if (["*", "/"].includes(val) && expression === '') {
+            return
+        }
+        if (["+", "*", "/"].includes(expression[expression.length - 1]) && val === "-") {
+            console.log('hi')
+        } else if (val === "(" && !["+", "-", "*", "/", "%","(", "."].includes(expression[expression.length - 1])) {
+          return
+        } else if (val == '(') {
+          parStack.push(val)
+            console.log('hi')
+        } else if (["+", "-", "*", "/", "%", "."].includes(expression[expression.length - 1])) {
+            return
+        }
+        setExpression(expression + val)
     }
-      setExpression(expression + val)
-  }
     return (
         <div className='calc-container'>
             <div className="res">
@@ -25,20 +58,19 @@ const Calcul: React.FC = () => {
                     {expression || '0'}
                 </p>
                 <p className="ev">
-                    {Math.round(Number(eval(ev)))}
+                    {ev}
                 </p>
 
             </div>
             <div className="operators">
                 <button className="op" onClick={() => {
                     setExpression('')
-                    setEv('0')
+                    setEv(0)
                     setLastNum('')
                 }}>Ac</button>
                 <button className="op" onClick={() => {
-                    setExpression('')
-                    setEv('0')
-                    setLastNum('')
+                    setExpression(expression.slice(0, expression.length - 1))
+                    setLastNum(lastNum.slice(0, lastNum.length - 1))
                 }}>del</button>
                 <button className="op" onClick={() => {
                     handleClick("%")
@@ -51,11 +83,22 @@ const Calcul: React.FC = () => {
 
                 <div className="numbers">
                     {
-                        [7,8,9,4,5,6,1,2,3,"00",0,"."].map((num, index) => {
+                        [7, 8, 9, 4, 5, 6, 1, 2, 3, 'special', 0, "."].map((num, index) => {
+                            if (num === 'special') {
+                                return (
+                                    <div key={index} className="paranthesis">
+                                        <button style={{ color: 'orange' }} onClick={() => {
+                                            handleClick('(')
+                                        }}>{"("}</button>
+                                        <button style={{ color: 'orange' }} onClick={() => {
+                                            handleClick(')')
+                                        }}>{")"}</button>
+                                    </div>
+                                )
+                            }
                             return (
                                 <button key={index} className="num" onClick={() => {
                                     setExpression(expression + num)
-                                    setEv(expression + num)
                                     setLastNum(lastNum + num)
                                 }}>{num}</button>
 
@@ -73,7 +116,7 @@ const Calcul: React.FC = () => {
                     <button className="op" onClick={() => {
                         handleClick("+")
                     }}>+</button>
-                    <button style={{color:'orange'}} className='op' onClick={() => {
+                    <button style={{ color: 'orange' }} className='op' onClick={() => {
                         setExpression(eval(expression))
                     }}>=</button>
                 </div>
