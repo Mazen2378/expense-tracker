ce83e857bcdf1de369a6788ac23c7ee6c237d95e
Mazen2378 <mazen.jedidi.23@gmail.com>
2022-10-27 21:01:37 +0100

    minor changes

--- src/components/RecentTransactions.tsx
+++ src/components/RecentTransactions.tsx
@@ -1,14 +1,19 @@
-import { format, getDate, getHours, getISODay, getMinutes, getMonth, getTime, getYear } from 'date-fns';
-import getDayOfYear from 'date-fns/fp/getDayOfYear/index';
+import { format} from 'date-fns';
 import React, {useContext} from 'react'
 import { BalanceContext } from '../context/BlanceContext';
 
 
-const RecentTransactions:React.FC = () => {
-    const { transactions } = useContext(BalanceContext)
+const RecentTransactions:React.FC = (e,) => {
+    const { transactions, setTransactions } = useContext(BalanceContext)
+  const handleClick:React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    console.log("hi")
+  }
   return (
     <>
           <div className='dash-panel'>
+            <h1>
+              Last Transactions
+            </h1>
               {/* <h1>income</h1>
               {transactions.income.map((t,index) => {
                   return (
@@ -22,21 +27,55 @@ const RecentTransactions:React.FC = () => {
                     </div>
                   )
               })} */}
-              <h1>outcome</h1>
-              {transactions.outcome.map((t,index) => {
+              {transactions.income.map((t,index) => {
                 const date = new Date(t.date)
                   return (
-                      <div className="eexpense-container" key={index}>
+                      <div className="expense-container" key={index}>
+                        <button onClick={()=> {
+                              const newIncome = transactions.income.filter((trans)=>trans.id !== t.id )
+                          setTransactions({...transactions,income:newIncome})
+
+                        }}>x</button>
                           <p>
                               {t.description}
                           </p>
+                          <div className="description">
+                              <p className='negative'>
+                                  {
+                                   t.amount % 1 === 0 ? `${t.amount}.00`: t.amount
+                                  }
+                              </p>
+                              <p>
+                                  {format(date, "yyy.mm.dd | hh:mm aaaaa.'m'")}
+                                  {/* {`${getDate(new Date(date))}.${getMonth(date)}.${getYear(date)} | ${getHours(date)}:${getMinutes(date)} ${get(date)}`} */}
+                              </p>
+                          </div>
+                      </div>
+                  )
+              })}
+              {transactions.outcome.map((t,index) => {
+                const date = new Date(t.date)
+                  return (
+                      <div className="expense-container" key={index}>
+                        <button onClick={()=> {
+                              const newOutcome = transactions.outcome.filter((trans)=>trans.id !== t.id )
+                          setTransactions({...transactions,outcome:newOutcome})
+
+                        }}>x</button>
                           <p>
-                              {t.amount}
+                              {t.description}
                           </p>
-                          <p>
-                              {format(new Date(), "yyy.mm.dd | hh:mm aaaaa.'m'")}
-                              {/* {`${getDate(new Date(date))}.${getMonth(date)}.${getYear(date)} | ${getHours(date)}:${getMinutes(date)} ${get(date)}`} */}
-                          </p>
+                          <div className="description">
+                              <p className='negative'>
+                                  {
+                                   t.amount % 1 === 0 ? `${t.amount}.00`: t.amount
+                                  }
+                              </p>
+                              <p>
+                                  {format(new Date(), "yyy.mm.dd | hh:mm aaaaa.'m'")}
+                                  {/* {`${getDate(new Date(date))}.${getMonth(date)}.${getYear(date)} | ${getHours(date)}:${getMinutes(date)} ${get(date)}`} */}
+                              </p>
+                          </div>
                       </div>
                   )
               })}
